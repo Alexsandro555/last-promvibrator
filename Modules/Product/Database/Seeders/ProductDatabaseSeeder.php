0b39a143967ed3ae8616c1ec5b5fc29dc9510482
alexsandro <alexsandro@oooleader.ru>
2019-03-12 15:48:08 +0300

    upd

--- Modules/Product/Database/Seeders/ProductDatabaseSeeder.php
+++ Modules/Product/Database/Seeders/ProductDatabaseSeeder.php
@@ -15,8 +15,8 @@ class ProductDatabaseSeeder extends Seeder
   public function run()
   {
     Model::unguard();
-
-    $this->call(TnvedTableSeeder::class);
+    $this->call(AttributeTableSeeder::class);
+    /*$this->call(TnvedTableSeeder::class);
     $this->call(ProducerTableSeeder::class);
     $this->call(ProductCategoryTableSeeder::class);
     $this->call(TypeProductTableSeeder::class);
@@ -24,6 +24,6 @@ class ProductDatabaseSeeder extends Seeder
     $this->call(AttributeTypeTableSeeder::class);
     $this->call(AttributeUnitTableSeeder::class);
     $this->call(AttributeGroupTableSeeder::class);
-    $this->call(VibratorsTableSeeder::class);
+    $this->call(VibratorsTableSeeder::class);*/
   }
 }
