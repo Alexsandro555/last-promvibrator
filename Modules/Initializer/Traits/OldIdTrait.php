efe3d31a429b00d65030d29637c7df55ab5195d8
alexsandro <alexsandro@oooleader.ru>
2019-03-13 16:10:04 +0300

    dasd

--- Modules/Initializer/Traits/OldIdTrait.php
+++ Modules/Initializer/Traits/OldIdTrait.php
@@ -4,7 +4,7 @@ namespace Modules\Initializer\Traits;
 
 trait OldIdTrait {
   protected static function bootOldIdTrait() {
-    static::created(function($model) {
+    static::updating(function($model) {
       if(!$model->old_id) {
         $model->old_id = $model->id;
       }
