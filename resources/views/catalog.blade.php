f8e3c2d58ed387e5d1369321370664717c9027a0
alexsandro <alexsandro@oooleader.ru>
2019-03-15 09:09:14 +0300

    fix bug url catalog

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -7,7 +7,7 @@
     <div class="content-left-menu">
       <a href="/{{$typeProduct->url_key}}" class="partition">{{$typeProduct->title}}</a>
       @foreach($typeProduct->lineProducts as $lineProduct)
-          <a href="{{$lineProduct->url_key}}">
+          <a href="/{{$lineProduct->url_key}}">
             {{$lineProduct->title}}
             <img src="{{asset('css/images/hover-menu.png')}}" alt="arrow"/>
           </a>
