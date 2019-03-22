229ca0b6266038aa092c0ee7b4e5e0e4ecaf8e8b
alexsandro <alexsandro@oooleader.ru>
2019-03-22 16:17:46 +0300

    frrf

--- resources/views/sidebar_nested_menu.blade.php
+++ resources/views/sidebar_nested_menu.blade.php
@@ -2,10 +2,12 @@
   <div class="content-left-menu">
     <a href="/{{$typeProduct->url_key}}" class="partition">{{$typeProduct->title}}</a>
     @foreach($typeProduct->lineProducts as $lineProduct)
-      <a href="/{{$lineProduct->url_key}}">
-        {{$lineProduct->title}}
-        <img src="{{asset('css/images/hover-menu.png')}}" alt="arrow"/>
-      </a>
+      @if($lineProduct->active)
+        <a href="/{{$lineProduct->url_key}}">
+          {{$lineProduct->title}}
+          <img src="{{asset('css/images/hover-menu.png')}}" alt="arrow"/>
+        </a>
+      @endif
     @endforeach
   </div>
 @endforeach
