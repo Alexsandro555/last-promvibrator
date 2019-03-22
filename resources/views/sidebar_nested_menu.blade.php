cc41f1a474f8f2b2246f177b4c2673cb6f87471f
alexsandro <alexsandro@oooleader.ru>
2019-03-22 16:24:59 +0300

    fix active

--- resources/views/sidebar_nested_menu.blade.php
+++ resources/views/sidebar_nested_menu.blade.php
@@ -1,13 +1,15 @@
 @foreach($productCategory->typeProducts as $typeProduct)
   <div class="content-left-menu">
-    <a href="/{{$typeProduct->url_key}}" class="partition">{{$typeProduct->title}}</a>
-    @foreach($typeProduct->lineProducts as $lineProduct)
-      @if($lineProduct->active)
-        <a href="/{{$lineProduct->url_key}}">
-          {{$lineProduct->title}}
-          <img src="{{asset('css/images/hover-menu.png')}}" alt="arrow"/>
-        </a>
-      @endif
-    @endforeach
+    @if($typeProduct->active)
+      <a href="/{{$typeProduct->url_key}}" class="partition">{{$typeProduct->title}}</a>
+      @foreach($typeProduct->lineProducts as $lineProduct)
+        @if($lineProduct->active)
+          <a href="/{{$lineProduct->url_key}}">
+            {{$lineProduct->title}}
+            <img src="{{asset('css/images/hover-menu.png')}}" alt="arrow"/>
+          </a>
+        @endif
+      @endforeach
+    @endif
   </div>
 @endforeach
