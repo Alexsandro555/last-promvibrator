743f4fda30144b13e450593c1105e51f16fca1a7
alexsandro <alexsandro@oooleader.ru>
2019-03-26 10:55:30 +0300

    fix search bug 2

--- app/Http/Controllers/FindController.php
+++ app/Http/Controllers/FindController.php
@@ -10,7 +10,7 @@ class FindController extends Controller
   public function index($text='')
   {
     $text = str_replace("_", "/", $text);
-    $products = Product::with('files', 'productCategory', 'attributes')->whereRaw("MATCH (title,description) AGAINST ('\"".$text."\"' IN BOOLEAN MODE) OR title LIKE '%".$text."%'")->where(active,1)->paginate(21);
+    $products = Product::with('files', 'productCategory', 'attributes')->where('active',1)->whereRaw("MATCH (title,description) AGAINST ('\"".$text."\"' IN BOOLEAN MODE) OR title LIKE '%".$text."%'")->paginate(21);
     return view('find', compact('products', 'text'));
   }
 }
